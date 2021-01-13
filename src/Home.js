2bc711d3a36f963d0a28b03fa6441352d8393673
AmbyMbayi <ambymbayi54@gmail.com>
2021-01-13 21:30:16 +0300

    click events added

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    const clickMe =(e) => {
+        console.log('click Me', e)
+    }
+    const clickMeAgain =(name, e) => {
+        console.log("Hello its me again", name, e.target)
+    }
     return (  
         <div className="home">
             <h2>Homepage</h2>
+           <button onClick={clickMe}>Click Me</button>
+           <button onClick={(e) => {clickMeAgain("Ken",e)}}>Click Me again</button>
         </div>
     );
 }
