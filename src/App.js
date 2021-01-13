b557ff8db53745ba5093f0e124cac594afc48b5f
AmbyMbayi <ambymbayi54@gmail.com>
2021-01-13 18:10:04 +0300

    components created on issue3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 
 import './App.css';
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
-  const title = "Hello Welcome to React icodeff blsog"
-  const likes = 50
-  const first_name = "Ambrose"
-  const person = {name: "Kev", age: "40"}
-  const link_site = "www.google.com"
-  const branch = "iss2"
-  
-  const last_name ="Amby"
+ 
+
   return (
     <div className="App">
-      <h1>{title}</h1>
-      <p>{likes} times</p>
-      <p>Hi {person.name} </p>
-      <p>You are {person.age} years</p>
-      <p>{first_name}</p>
-      <a href={link_site}>Google site</a>
-      <p>{branch}</p>
-      
-      <p>{last_name}</p>
+      <Navbar/>
+     <div className="content">
+      <Home/>
+     </div>
     </div>
   );
 }
