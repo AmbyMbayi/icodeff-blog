84b08cf45683ffca7b6173bb5151379f7ea21371
AmbyMbayi <ambymbayi54@gmail.com>
2021-01-13 17:34:06 +0300

    changes on main

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   const title = "Hello Welcome to React icodeff blsog"
   const likes = 50
+  const first_name = "Ambrose"
   const person = {name: "Kev", age: "40"}
   const link_site = "www.google.com"
   return (
@@ -12,6 +13,7 @@ function App() {
       <p>{likes} times</p>
       <p>Hi {person.name} </p>
       <p>You are {person.age} years</p>
+      <p>{first_name}</p>
       <a href={link_site}>Google site</a>
     </div>
   );
