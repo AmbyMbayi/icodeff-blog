c2e55a78a2d0fb9b1388c050ffea3af03445a735
AmbyMbayi <ambymbayi54@gmail.com>
2021-01-14 08:42:43 +0300

    using props on iss8

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,18 @@
 import {useState} from 'react'
+import BlogList from './BlogList'
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        {author: "Amby Mbayi",title: "Title One", body: "body of title one...", id: 1},
-    ])
-    
+   const [blogs, setBlogs] = useState([
+       {title: "Title One", body:"Body of title one", author: "Amby", id: 1},
+       {title: "title Two", body: "Body of title two", author: "Mbayi", id: 2},
+       {title: "title Three", body: "Body of title three", author: "Ben", id: 3},
+       {title: "title Four", body: "body of title four", author: "Achwal", id: 4}
+   ])
     return (  
         <div className="home">
-          
-     {blogs.map((blog) =>(
-         <div className="blog-preview" key={blog.id}>
-             <h2>{blog.title}</h2>
-             <p>Written by {blog.author}</p>
-         </div>
-     ))}
+         
+        <BlogList blogs ={blogs} title="List of Blogs" />
            
         </div>
     );
