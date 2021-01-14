94c36b391c0e62646f5d0725b16a55216857ffd4
AmbyMbayi <ambymbayi54@gmail.com>
2021-01-14 08:50:36 +0300

    resusing blog component and filter method on iss9

--- src/Home.js
+++ src/Home.js
@@ -6,13 +6,14 @@ const Home = () => {
    const [blogs, setBlogs] = useState([
        {title: "Title One", body:"Body of title one", author: "Amby", id: 1},
        {title: "title Two", body: "Body of title two", author: "Mbayi", id: 2},
-       {title: "title Three", body: "Body of title three", author: "Ben", id: 3},
+       {title: "title Three", body: "Body of title three", author: "Amby", id: 3},
        {title: "title Four", body: "body of title four", author: "Achwal", id: 4}
    ])
     return (  
         <div className="home">
          
         <BlogList blogs ={blogs} title="List of Blogs" />
+        <BlogList blogs = {blogs.filter((blog) => blog.author=== "Amby") } title="Amby's Posts" />
            
         </div>
     );
