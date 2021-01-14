7d5190aadab1bbbe27eec7e5474b398ef3bd7ca8
AmbyMbayi <ambymbayi54@gmail.com>
2021-01-14 13:12:55 +0300

    Handling Errors using catch and setting them on state

--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,27 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((response) => {
+          console.log(response);
+          if (!response.ok) {
+            throw Error("There is an error");
+          }
+
           return response.json();
         })
         .then((data) => {
           setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          console.log(err.message);
+          setError(err.message);
           setIsPending(false);
         });
     }, 1000);
@@ -20,6 +32,7 @@ const Home = () => {
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
