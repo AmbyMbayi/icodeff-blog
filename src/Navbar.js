5eed74e2d13acc1ef4a1080a0d9f53e06dde85f5
AmbyMbayi <ambymbayi54@gmail.com>
2021-01-18 11:46:50 +0300

    react exact router match

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
-    return ( 
-        <nav className="navbar">
-            <h1>icodeff Blog</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">Create New Blog</a>
-            </div>
-        </nav>
-     );
-}
- 
+  return (
+    <nav className="navbar">
+      <h1>icodeff Blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create">Create New Blog</Link>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
