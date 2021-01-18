d7542adbce6eecb651db4bc074ad2959d592cc4a
AmbyMbayi <ambymbayi54@gmail.com>
2021-01-18 11:59:20 +0300

    useEffect cleanup added

--- src/useFetch.js
+++ src/useFetch.js
@@ -5,8 +5,10 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then((response) => {
           console.log(response);
           if (!response.ok) {
@@ -21,11 +23,16 @@ const useFetch = (url) => {
           setError(null);
         })
         .catch((err) => {
-          console.log(err.message);
-          setError(err.message);
-          setIsPending(false);
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
         });
     }, 1000);
+
+    return () => abortCont.abort();
   }, [url]);
 
   return { data, isPending, error };
