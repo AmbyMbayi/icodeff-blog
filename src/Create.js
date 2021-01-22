0b730647e5394980ca61d0098370f3a551c1a35c
AmbyMbayi <ambymbayi54@gmail.com>
2021-01-22 11:29:20 +0300

    using controll inputs and state to capture data and display

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Amby");
+
   return (
     <div className="create">
       <h2>Create Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <label for="">Blog title:</label>
+        <textarea
+          name="title"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          required
+        ></textarea>
+        <label>BLog Author: </label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="amby">amby</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
   );
 };
