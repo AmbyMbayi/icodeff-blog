11f81eafbd3eba48a93be2475116bc28cf64e898
AmbyMbayi <ambymbayi54@gmail.com>
2021-01-22 14:29:30 +0300

    notfound page added

--- src/Notfound.js
+++ src/Notfound.js
@@ -4,7 +4,9 @@ const Notfound = () => {
     <div className="not-found">
       <h2>Sorry</h2>
       <p>That page cannot be found</p>
-      <Link to="/">Back to the Home page...</Link>
+      <button>
+        <Link to="/">Back to the Home page...</Link>
+      </button>
     </div>
   );
 };
